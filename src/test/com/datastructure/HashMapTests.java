dfcb19c470c53f5fc0c70d9ae78b4d91290d7476
Sahil <sahilghanghas93@gmail.com>
2019-06-13 04:34:30 -0400

    Update HashMapTests.java

--- src/test/com/datastructure/HashMapTests.java
+++ src/test/com/datastructure/HashMapTests.java
@@ -11,22 +11,46 @@ public class HashMapTests {
 	@Test
 	public void insetTest() {
 		HashMap<String, Integer>map = new HashMap<>(); 
-        map.add("this",1 ); 
-        map.add("coder",2 ); 
-        map.add("this",4 ); 
-        map.add("hi",5 ); 
+        map.add("testing",1 ); 
+        map.add("hash",2 );
+        map.add("map",5 ); 
         assertEquals(3,map.getSize());
 	}
 	
 	@Test
 	public void removeTest() {
 		HashMap<String, Integer>map = new HashMap<>(); 
-        map.add("this",1 ); 
-        map.add("coder",2 ); 
-        map.add("this",4 ); 
-        map.add("hi",5 );
-        map.remove("this");
+        map.add("testing",1 ); 
+        map.add("hash",2 ); 
+        map.add("testing",4 ); 
+        map.add("map",5 );
+        map.remove("testing");
         assertEquals(2,map.getSize());
+	}
+	
+	@Test
+	public void getTest() {
+		HashMap<String, Integer>map = new HashMap<>(); 
+        map.add("testing",1 ); 
+        map.add("hash",2 ); 
+        map.add("map",5 );
+        assertEquals(2,map.get("hash"));
+	}
+	
+	@Test
+	public void containsTest() {
+		HashMap<String, Integer>map = new HashMap<>(); 
+        map.add("testing",1 ); 
+        map.add("hash",2 ); 
+        map.add("map",5 );
+        assertEquals(true,map.contains("hash"));
+        assertEquals(false,map.contains("table"));
+	}
+	
+	@Test
+	public void emptyTest() {
+		HashMap<String, Integer>map = new HashMap<>(); 
+        assertEquals(true,map.isEmpty());
 	}
 	
 }
