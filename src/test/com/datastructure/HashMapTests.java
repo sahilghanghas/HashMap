91afdc36123999bc1c800b81e1b54af793309232
Sahil <sahilghanghas93@gmail.com>
2019-06-13 04:35:07 -0400

    Update HashMapTests.java

--- src/test/com/datastructure/HashMapTests.java
+++ src/test/com/datastructure/HashMapTests.java
@@ -2,7 +2,6 @@ package test.com.datastructure;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertNotNull;
 
 import com.datastructure.HashMap;
 
